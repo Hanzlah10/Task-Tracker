f2b881ebaaaff056567b1ba3fa585b0f0b6fcd53
Hanzala Sarguroh <hanzalasarguroh@gmail.com>
2024-12-17 01:44:50 +0530

    Add login and register components with basic templates and styles; configure Tailwind CSS

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,25 +5,13 @@ import bodyParser from 'body-parser';
 import { taskRouter } from './routes/task.routes.js';
 
 const app = express();
-
-
-
 app.use('/static', express.static('public'));
-
-const printHello = (req, res) => {
-    console.log(req.body);
-    res.send("HEllo")
-}
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.get('/', printHello)
 
-
-// routes
 
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/tasks', taskRouter)
-
 
 export { app }
